02213d8e2196936bb5ede113448c2f039b3aaa70
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-01-25 17:06:04 +0100

    broadcast message to channel 2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,11 +22,10 @@ class MessagesController < ApplicationController
   # POST /messages or /messages.json
   def create
     @message = Message.new(message_params)
-
     @message.user = current_user
-    
     @message.save
-    redirect_to request.referrer
+
+    ActionCable.server.broadcast "room_channel_#{@message.room_id}", message: @message.content
   end
 
   # PATCH/PUT /messages/1 or /messages/1.json
