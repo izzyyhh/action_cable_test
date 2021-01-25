9f8701b6a51ba1a52087dface37bb03a840e94a3
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-01-25 17:55:00 +0100

    add channel specific message, right channel

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,17 +1,25 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create( {channel: "RoomChannel", room_id: 2}, {
-  connected() {
-    console.log("connected")
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener('turbolinks:load', () => {
+  const userIdEl = document.getElementById('user-id')
+  const roomIdEl = document.getElementById('room-id')
+  
+  const userId = Number(userIdEl.getAttribute('data-user-id'))
+  const roomId = Number(roomIdEl.getAttribute('data-room-id'))
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    console.log(data)
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+  consumer.subscriptions.create( {channel: "RoomChannel", room_id: roomId}, {
+    connected() {
+      console.log("connected to " + roomId)
+      // Called when the subscription is ready for use on the server
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      console.log(data)
+      // Called when there's incoming data on the websocket for this channel
+    }
+  });
+})
